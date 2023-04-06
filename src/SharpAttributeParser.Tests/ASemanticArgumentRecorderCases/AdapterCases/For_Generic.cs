08087a384b550dece8cd870ce348f1aa335ad540
ErikWe <erikwestberg7@gmail.com>
2023-04-06 03:03:22 +0200

    Add test-cases for generic adapter methods

---
+++ src/SharpAttributeParser.Tests/ASemanticArgumentRecorderCases/AdapterCases/For_Generic.cs
@@ -0,0 +1,46 @@
+﻿namespace SharpAttributeParser.Tests.ASemanticArgumentRecorderCases.AdapterCases;
+
+using Microsoft.CodeAnalysis;
+
+using Moq;
+
+using SharpAttributeParser;
+
+using System.Collections.Generic;
+
+using Xunit;
+
+public class For_Generic
+{
+    private static bool TryRecordGenericArgument(ASemanticArgumentRecorder recorder, string parameterName, ITypeSymbol value) => recorder.TryRecordGenericArgument(parameterName, value);
+
+    [Fact]
+    public void True_RecordedPopulated()
+    {
+        Recorder recorder = new();
+
+        var parameterName = "Value";
+        Mock<ITypeSymbol> symbolMock = new();
+
+        var actual = TryRecordGenericArgument(recorder, parameterName, symbolMock.Object);
+
+        Assert.True(actual);
+
+        Assert.Equal(symbolMock.Object, recorder.Value);
+    }
+
+    private sealed class Recorder : ASemanticArgumentRecorder
+    {
+        public ITypeSymbol? Value { get; private set; }
+
+        protected override IEnumerable<(string, DSemanticGenericRecorder)> AddGenericRecorders()
+        {
+            yield return ("Value", Adapters.For(RecordValue));
+        }
+
+        private void RecordValue(ITypeSymbol value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ src/SharpAttributeParser.Tests/ASyntacticArgumentRecorderCases/AdapterCases/For_Generic.cs
@@ -0,0 +1,46 @@
+﻿namespace SharpAttributeParser.Tests.ASyntacticArgumentRecorderCases.AdapterCases;
+
+using Microsoft.CodeAnalysis;
+
+using Moq;
+
+using SharpAttributeParser;
+
+using System.Collections.Generic;
+
+using Xunit;
+
+public class For_Generic
+{
+    private static bool TryRecordGenericArgument(ASyntacticArgumentRecorder recorder, string parameterName, ITypeSymbol value) => recorder.TryRecordGenericArgument(parameterName, value, Location.None);
+
+    [Fact]
+    public void True_RecordedPopulated()
+    {
+        Recorder recorder = new();
+
+        var parameterName = "Value";
+        Mock<ITypeSymbol> symbolMock = new();
+
+        var actual = TryRecordGenericArgument(recorder, parameterName, symbolMock.Object);
+
+        Assert.True(actual);
+
+        Assert.Equal(symbolMock.Object, recorder.Value);
+    }
+
+    private sealed class Recorder : ASyntacticArgumentRecorder
+    {
+        public ITypeSymbol? Value { get; private set; }
+
+        protected override IEnumerable<(string, DSyntacticGenericRecorder)> AddGenericRecorders()
+        {
+            yield return ("Value", Adapters.For(RecordValue));
+        }
+
+        private void RecordValue(ITypeSymbol value, Location location)
+        {
+            Value = value;
+        }
+    }
+}
