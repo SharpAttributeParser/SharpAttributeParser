1eba4f6485ac1d627bf19faeefcdf815d6b4c347
ErikWe <erikwestberg7@gmail.com>
2023-04-06 03:00:42 +0200

    Added recorder delegates to .Abstractions project

---
+++ src/SharpAttributeParser.Abstractions/DSemanticArrayRecorder.cs
@@ -0,0 +1,8 @@
+﻿namespace SharpAttributeParser;
+
+using System.Collections.Generic;
+
+/// <summary>Responsible for recording the semantically parsed array-valued argument of a constructor or named parameter.</summary>
+/// <param name="value">The value of the argument.</param>
+/// <returns>A <see cref="bool"/> indicating whether the argument was successfully recorded.</returns>
+public delegate bool DSemanticArrayRecorder(IReadOnlyList<object?>? value);
---
+++ src/SharpAttributeParser.Abstractions/DSyntacticArrayRecorder.cs
@@ -0,0 +1,15 @@
+﻿namespace SharpAttributeParser;
+
+using Microsoft.CodeAnalysis;
+
+using System;
+using System.Collections.Generic;
+
+/// <summary>Responsible for recording the syntactically parsed array-valued argument of a constructor or named parameter.</summary>
+/// <param name="value">The value of the argument.</param>
+/// <param name="collectionLocation">The <see cref="Location"/> of the entire argument.</param>
+/// <param name="elementLocations">The <see cref="Location"/> of the individual elements in the array-valued argument.</param>
+/// <returns>A <see cref="bool"/> indicating whether the argument was successfully recorded.</returns>
+/// <exception cref="ArgumentException"/>
+/// <exception cref="ArgumentNullException"/>
+public delegate bool DSyntacticArrayRecorder(IReadOnlyList<object?>? value, Location collectionLocation, IReadOnlyList<Location> elementLocations);
